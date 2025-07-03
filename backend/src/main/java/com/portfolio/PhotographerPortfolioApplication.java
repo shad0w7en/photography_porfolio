14c87abb1032adf61ee858110ac6015b36a4e733
shad0w7en <ankuryadav93932@gmail.com>
2025-07-03 22:05:48 +0000

    added commnet

--- backend/src/main/java/com/portfolio/PhotographerPortfolioApplication.java
+++ backend/src/main/java/com/portfolio/PhotographerPortfolioApplication.java
@@ -3,6 +3,7 @@ package com.portfolio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//added comment
 @SpringBootApplication
 public class PhotographerPortfolioApplication {
 
